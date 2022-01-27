1d45e1c63fda70b745360af0a1def2604e40201b
ajitnitr <92538695+ajitnitr@users.noreply.github.com>
2022-01-27 17:34:02 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "terraform-b61"
+    bucket = "terraform-bkt2022"
     key    = "vpc/${var.ENV}/terraform.tfstate"
     region = "us-east-1"
   }
